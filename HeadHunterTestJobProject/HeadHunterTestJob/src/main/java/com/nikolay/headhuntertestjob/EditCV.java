e38ae805e537d693cdb26fc4decb176dd67f8997
NikolayMarkin <markoni.nik@gmail.com>
2013-09-11 00:39:03 +0400

    добавлен компонент выбора даты

--- HeadHunterTestJobProject/HeadHunterTestJob/src/main/java/com/nikolay/headhuntertestjob/EditCV.java
+++ HeadHunterTestJobProject/HeadHunterTestJob/src/main/java/com/nikolay/headhuntertestjob/EditCV.java
@@ -1,32 +1,77 @@
 package com.nikolay.headhuntertestjob;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class EditCV extends Activity {
 
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+
+    private EditText dateOfBirthEditText;
+
     @Override
-    public void onCreate(Bundle savedInstaceState) {
-        super.onCreate(savedInstaceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.new_cv);
+
+        dateOfBirthEditText = (EditText) findViewById(R.id.dateOfBirthEditText);
+
+        dateOfBirthEditText.setOnClickListener(dateOfBirthTextListener);
 
         Button sendCVButton = (Button) findViewById(R.id.sendCVButton);
         sendCVButton.setOnClickListener(sendCVButtonClicked);
     }
 
-    OnClickListener sendCVButtonClicked = new OnClickListener() {
+    OnClickListener dateOfBirthTextListener = new OnClickListener() {
         @Override
         public void onClick(View view) {
 
+            Calendar calendar = Calendar.getInstance();
+
+            DatePickerDialog datePickerDialog =
+                    new DatePickerDialog(EditCV.this,
+                            onDateSetListener,
+                            calendar.get(Calendar.YEAR),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH)
+                    );
+
+            datePickerDialog.show();
+        }
+    };
+
+    DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            mYear = year;
+            mMonth = month;
+            mDay = day;
+
+            Date chosedDate = new Date(year, month, day);
+
+            dateOfBirthEditText.setText(new SimpleDateFormat("yyyy-MM-dd").format(chosedDate));
+        }
+    };
+
+    OnClickListener sendCVButtonClicked = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
             // преключиться к просмотру резюме
             Intent viewCV = new Intent(EditCV.this, ViewCV.class);
-
             startActivity(viewCV);
-
         }
     };
 }
